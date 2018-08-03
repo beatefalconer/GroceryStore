f6c17c075b6dacdb6f107b05f784cb89e1a3235d
bfalcone <beate_falconer@yahoo.com>
2018-08-03 09:27:46 -0700

    pull total cost calculation out of main program
    in order to be able to do better unit tests

---
+++ Register.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore1
+{
+    public class Register
+    {
+        int priceA = 30;
+        int priceB = 50;
+        int special = 4;
+
+        public double calculateTotal(int totalA, int totalB)
+        {
+            double totalCost;
+            totalCost = priceA * totalA + priceB * (totalB / (special + 1)) * special + priceB * (totalB % (special + 1));
+            //totalCost = totalCost / 100;
+            return totalCost;
+        }
+    }
+}
