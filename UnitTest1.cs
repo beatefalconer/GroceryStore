d3f3801535e8afd58b37b69d6f416b8d3254ec39
bfalcone <beate_falconer@yahoo.com>
2018-08-03 09:26:42 -0700

    first Unit tests with Ranjeewa
    check price of Apples and Bananas

---
+++ UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryStore1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsApple30()
+        {
+            Register register1 = new Register(); //arrange
+            double cost = register1.calculateTotal(1, 0); //act
+            Assert.AreEqual(30, cost, 0.001, "one Apple is not 30"); //assert
+        }
+        [TestMethod]
+        public void IsBanana50()
+        {
+            //
+            Register register2 = new Register(); //arrange
+            double cost = register2.calculateTotal(0, 1); //act
+            Assert.AreEqual(50, cost, 0.001, "one Banana is not 50"); //assert
+        }
+    }
+}
